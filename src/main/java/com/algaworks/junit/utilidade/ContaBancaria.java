5d9859870d86deb95abb0e0a15ffa230e3caf39c
allcantara <brunoalcantarajc@gmail.com>
2025-02-26 14:08:51 -0300

    Implementa as regras da conta bancária

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -2,26 +2,54 @@ package com.algaworks.junit.utilidade;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ZERO;
+import static java.util.Objects.isNull;
+
 public class ContaBancaria {
+
+    private BigDecimal saldoConta;
 
     public ContaBancaria(BigDecimal saldo) {
         //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - pode ser zero ou negativo
+
+        if (isNull(saldo)) {
+            throw new IllegalArgumentException("O saldo não pode ser null");
+        }
+
+        this.saldoConta = saldo;
     }
 
     public void saque(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - Deve subtrair o valor do saldo
         //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+
+        validarValorSolicitado(valor);
+
+        if (this.saldoConta.compareTo(valor) < 0) {
+            throw new RuntimeException("Saldo insuficiente");
+        }
+
+        this.saldoConta = this.saldoConta.subtract(valor);
     }
 
     public void deposito(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - Deve adicionar o valor ao saldo
+
+        validarValorSolicitado(valor);
+        this.saldoConta = this.saldoConta.add(valor);
+    }
+
+    private static void validarValorSolicitado(BigDecimal valor) {
+        if (isNull(valor) || valor.compareTo(ZERO) <= 0) {
+            throw new IllegalArgumentException("O saldo não pode ser null ou menor/igual a zero");
+        }
     }
 
     public BigDecimal saldo() {
         //TODO 1 - retornar saldo
-        return null;
+        return this.saldoConta;
     }
 }
